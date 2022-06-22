375c0c9d23908345ef09bbd481328f167e4788cd
lexissanders <amsanders99@yahoo.com>
2022-06-22 02:42:07 -0500

    text alignment and text color

--- Two-Button-Challenge/ViewController.swift
+++ Two-Button-Challenge/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are awesome"
+        messageLabel.textColor = UIColor.blue
+        messageLabel.textAlignment = NSTextAlignment.left
+    }
+    
+    @IBAction func showAnotherMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are great"
+        messageLabel.textColor = .systemRed
+        messageLabel.textAlignment = NSTextAlignment.right
+    }
+    
 }
 
